d1e1aa1fdff7d9ade038f9e1c83f32c558143110
Erreur-92 <evan.verplaetse@epitech.eu>
2025-01-18 18:55:51 +0100

    [Deploy]

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // Si tu as un fichier CSS
+import App from './App'; // Le composant principal de l'application
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
