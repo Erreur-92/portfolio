f4877dc285fd7f3541e4c1688e84bd6f6ed4fa53
Erreur-92 <evan.verplaetse@epitech.eu>
2025-02-20 19:35:35 +0100

    [RESET]

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,280 +1,175 @@
 import React, { useEffect, useState } from 'react';
 import './Home.css';
+import presImg from './image/image.jpg'; // Ensure the correct path to the image
+import aboutImg from './image/aboutImg.jpg'; // Ensure the correct path to the image
 
 function Home() {
-  const [text, setText] = useState('');
-  const [showCursor, setShowCursor] = useState(true);
-  const [showCards, setShowCards] = useState(false);
-  const [showCarte, setShowCarte] = useState(false);
-  const [isTerminalVisible, setIsTerminalVisible] = useState(true);
-  const [loginInput, setLoginInput] = useState('');
-  const [passwordInput, setPasswordInput] = useState('');
-  const [isLoginComplete, setIsLoginComplete] = useState(false);
-  const [loadingLines, setLoadingLines] = useState([]);
-  const [isSiteVisible, setIsSiteVisible] = useState(false);
-  const [isNavVisible, setIsNavVisible] = useState(false);
-  const [cpuUsage, setCpuUsage] = useState(0);
-  const [memoryUsage, setMemoryUsage] = useState(0);
-  const [currentTime, setCurrentTime] = useState(new Date());
-  const [pressedKey, setPressedKey] = useState(null);
-
-  const fullText = 'Bienvenue sur la page d\'accueil de mon portfolio. Ici, vous découvrirez mes projets, mon parcours et comment me contacter.';
-  const loginTarget = "admin";
-  const passwordTarget = "********";
-
-  useEffect(() => {
-    if (!isTerminalVisible) {
-      let index = 0;
-      const interval = setInterval(() => {
-        if (index < fullText.length) {
-          setText((prev) => prev + fullText[index]);
-          index++;
-        } else {
-          clearInterval(interval);
-        }
-      }, 50);
-
-      return () => clearInterval(interval);
-    }
-  }, [isTerminalVisible]);
-
-  useEffect(() => {
-    const cursorInterval = setInterval(() => {
-      setShowCursor((prev) => !prev);
-    }, 500);
-
-    return () => clearInterval(cursorInterval);
-  }, []);
-
-  useEffect(() => {
-    if (isLoginComplete) {
-      startLoadingSimulation();
-    }
-  }, [isLoginComplete]);
-
-  useEffect(() => {
-    simulateLogin();
-  }, []);
-
-  useEffect(() => {
-    const cpuInterval = setInterval(() => {
-      setCpuUsage(Math.floor(Math.random() * 100));
-      setMemoryUsage(Math.floor(Math.random() * 100));
-    }, 2000);
-
-    const timeInterval = setInterval(() => {
-      setCurrentTime(new Date());
-    }, 1000);
-
-    return () => {
-      clearInterval(cpuInterval);
-      clearInterval(timeInterval);
-    };
-  }, []);
-
-  const simulateLogin = () => {
-    let loginIndex = 0;
-    let passwordIndex = 0;
-
-    const loginInterval = setInterval(() => {
-      if (loginIndex < loginTarget.length) {
-        setLoginInput((prev) => prev + loginTarget[loginIndex]);
-        loginIndex++;
-      } else {
-        clearInterval(loginInterval);
-        const passwordInterval = setInterval(() => {
-          if (passwordIndex < passwordTarget.length) {
-            setPasswordInput((prev) => prev + passwordTarget[passwordIndex]);
-            passwordIndex++;
-          } else {
-            clearInterval(passwordInterval);
-            setIsLoginComplete(true);
-          }
-        }, 200);
-      }
-    }, 200);
-  };
-
-  const startLoadingSimulation = () => {
-    const lines = [
-      "initialisation_portfolio.exe772 23.4M/bits 100%",
-      "chargement_modules.css 1.1M/bits 100%",
-      "installation_fichiers_requis.js 3.2M/bits 100%",
-      "connexion_serveur_deployé 512K/bits 100%",
-      "synchronisation_données 8.3M/bits 100%",
-      "optimisation_ressources_systeme 5.4M/bits 100%",
-      "vérification_intégrité_systèmes 7.7M/bits 100%",
-      "chargement_interface_utilisateur 12.1M/bits 100%",
-      "déploiement_finalisation_portfolio.exe 15.6M/bits 100%",
-      "préparation_démarrage_serveur 10.3M/bits 100%",
-      "scan_memoires.viruscheck_safe 0.0%",
-      "cryptage_données_utilisateur 1.4M/bits 100%",
-      "mise_en_ligne_profil_technique 9.2M/bits 100%",
-      "exécution_script_final.js 20.3M/bits 100%",
-      "activation_interfaces_réseau 3.5M/bits 100%",
-      "connexion_au_serveur_principal 18.9M/bits 100%",
-      "chargement_terminal_hack_simulation 6.4M/bits 100%",
-      "execution_honeycomb_ui_loader 7.3M/bits 100%",
-      "startup_initial_sequence_check OK",
-      "portefeuille_déployé_succès.exe 100%",
-      " ",
-      ">>> démarrage_achevé_bienvenue_admin <<<",
-    ];
-
-    let index = 0;
-    const maxLines = 15;
-    const interval = setInterval(() => {
-      if (index < lines.length) {
-        setLoadingLines((prev) => {
-          const updatedLines = [...prev, lines[index]];
-          return updatedLines.length > maxLines ? updatedLines.slice(1) : updatedLines;
-        });
-        index++;
-      } else {
-        clearInterval(interval);
-        setTimeout(() => {
-          setIsTerminalVisible(false);
-          setTimeout(() => setIsSiteVisible(true), 500);
-        }, 1000);
-      }
-    }, 100);
-  };
-
-  const toggleNav = () => {
-    setIsNavVisible((prev) => !prev);
-  };
-
-  // Gestion des événements clavier physiques
-  useEffect(() => {
-    const handleKeyPress = (e) => {
-      const key = e.key.toUpperCase(); // Récupère la touche pressée et la met en majuscule
-      setPressedKey(key); // Enregistre la touche pressée
-      setTimeout(() => setPressedKey(null), 200); // Réinitialise après un court délai
-    };
-
-    window.addEventListener("keydown", handleKeyPress);
-
-    return () => {
-      window.removeEventListener("keydown", handleKeyPress);
-    };
-  }, []);
-
-  // Liste des touches en AZERTY, organisées en lignes
-  const keys = [
-    ['A', 'Z', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
-    ['Q', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M'],
-    ['W', 'X', 'C', 'V', 'B', 'N'],
-    ['Shift', 'Ctrl', 'Alt', 'Space', 'Entrée']
-  ];
-
-  // Fonction de gestion des clics sur les touches du clavier virtuel
-  const handleVirtualKeyPress = (key) => {
-    setPressedKey(key);
-    setTimeout(() => setPressedKey(null), 200); // Réinitialise après un court délai
-  };
-
   return (
-    <div className={`App ${isSiteVisible ? 'fade-in cathodic-effect' : ''}`}>
-      {isTerminalVisible ? (
-        <div id="terminal" className="terminal-fullscreen">
-          <div className="line">
-            Login: <span id="login">{loginInput}</span>
-          </div>
-          <div className="line">
-            Password: <span id="password">{passwordInput}</span>
-          </div>
-          {loadingLines.map((line, index) => (
-            <div key={index} className="line">
-              {line}
+    <div className="Home">
+          <div className="lander">
+            <div className="nav">
+              <div className="nav-text">
+                <p>Bienvenue</p>
+              </div>
+              <p className="cross">x</p>
             </div>
-          ))}
-          <div className="system-stats">
-            <div>CPU: {cpuUsage}%</div>
-            <div>MEM: {memoryUsage}%</div>
-            <div>Time: {currentTime.toLocaleTimeString('fr-FR', { hour12: false })}</div>
+            <img src={presImg} alt="Image de présentation" className="home-image" />
+            <div className="home-text">
+              <h1>Bienvenue sur mon Portfolio</h1>
+              <p>
+                Je m'appelle Evan Verplaetse. Ici, vous pourrez découvrir comment me contacter, retrouver mes projets et en
+                savoir plus sur moi !
+              </p>
+              <p>
+                Ce site Web cache quelque petits secrets. Je vous laisse explorer afin de découvrir toutes ses
+                fonctionnalitées cachées.
+              </p>
+              <p>Bonne chance !</p>
+            </div>
+            <div className="home-footer">
+              <div className="about-card">
+                <div className="about-text">
+                  <span>En savoir plus</span>
+                  <p>En cliquant sur ce bouton, vous pourrez en savoir un peu plus sur moi !</p>
+                </div>
+              </div>
+              <div className="contact-card">
+                <div className="contact-text">
+                  <span>Contact</span>
+                  <p>En cliquant sur ce bouton, vous trouverez les moyens de me contacter !</p>
+                </div>
+              </div>
+            </div>
+            <div className="project-section">
+              <h1>Mes projets</h1>
+            </div>
+            <div className="link-to-project">
+              <svg xmlns="http://www.w3.org/2000/svg" height="200" width="200">
+                <g style={{ order: -1 }}>
+                  <polygon
+                    transform="rotate(45 100 100)"
+                    strokeWidth="1"
+                    stroke="#d3a410"
+                    fill="none"
+                    points="70,70 148,50 130,130 50,150"
+                    id="bounce"
+                  ></polygon>
+                  <polygon
+                    transform="rotate(45 100 100)"
+                    strokeWidth="1"
+                    stroke="#d3a410"
+                    fill="none"
+                    points="70,70 148,50 130,130 50,150"
+                    id="bounce2"
+                  ></polygon>
+                  <polygon
+                    transform="rotate(45 100 100)"
+                    strokeWidth="2"
+                    stroke=""
+                    fill="#414750"
+                    points="70,70 150,50 130,130 50,150"
+                  ></polygon>
+                  <polygon
+                    strokeWidth="2"
+                    stroke=""
+                    fill="url(#gradiente)"
+                    points="100,70 150,100 100,130 50,100"
+                  ></polygon>
+                  <defs>
+                    <linearGradient y2="100%" x2="10%" y1="0%" x1="0%" id="gradiente">
+                      <stop style={{ stopColor: '#1e2026', stopOpacity: 1 }} offset="20%"></stop>
+                      <stop style={{ stopColor: '#414750', stopOpacity: 1 }} offset="60%"></stop>
+                    </linearGradient>
+                  </defs>
+                  <polygon
+                    transform="translate(20, 31)"
+                    strokeWidth="2"
+                    stroke=""
+                    fill="#b7870f"
+                    points="80,50 80,75 80,99 40,75"
+                  ></polygon>
+                  <polygon
+                    transform="translate(20, 31)"
+                    strokeWidth="2"
+                    stroke=""
+                    fill="url(#gradiente2)"
+                    points="40,-40 80,-40 80,99 40,75"
+                  ></polygon>
+                  <defs>
+                    <linearGradient y2="100%" x2="0%" y1="-17%" x1="10%" id="gradiente2">
+                      <stop style={{ stopColor: '#d3a51000', stopOpacity: 1 }} offset="20%"></stop>
+                      <stop style={{ stopColor: '#d3a51054', stopOpacity: 1 }} offset="100%" id="animatedStop"></stop>
+                    </linearGradient>
+                  </defs>
+                  <polygon
+                    transform="rotate(180 100 100) translate(20, 20)"
+                    strokeWidth="2"
+                    stroke=""
+                    fill="#d3a410"
+                    points="80,50 80,75 80,99 40,75"
+                  ></polygon>
+                  <polygon
+                    transform="rotate(0 100 100) translate(60, 20)"
+                    strokeWidth="2"
+                    stroke=""
+                    fill="url(#gradiente3)"
+                    points="40,-40 80,-40 80,85 40,110.2"
+                  ></polygon>
+                  <defs>
+                    <linearGradient y2="100%" x2="10%" y1="0%" x1="0%" id="gradiente3"></linearGradient>
+                      <stop style={{ stopColor: '#d3a51000', stopOpacity: 1 }} offset="20%"></stop>
+                      <stop style={{ stopColor: '#d3a51054', stopOpacity: 1 }} offset="100%" id="animatedStop"></stop>
+                  </defs>
+                  <polygon
+                    transform="rotate(45 100 100) translate(80, 95)"
+                    strokeWidth="2"
+                    stroke=""
+                    fill="#ffe4a1"
+                    points="5,0 5,5 0,5 0,0"
+                    id="particles"
+                  ></polygon>
+                  <polygon
+                    transform="rotate(45 100 100) translate(80, 55)"
+                    strokeWidth="2"
+                    stroke=""
+                    fill="#ccb069"
+                    points="6,0 6,6 0,6 0,0"
+                    id="particles"
+                  ></polygon>
+                  <polygon
+                    transform="rotate(45 100 100) translate(70, 80)"
+                    strokeWidth="2"
+                    stroke=""
+                    fill="#fff"
+                    points="2,0 2,2 0,2 0,0"
+                    id="particles"
+                  ></polygon>
+                  <polygon strokeWidth="2" stroke="" fill="#292d34" points="29.5,99.8 100,142 100,172 29.5,130"></polygon>
+                  <polygon
+                    transform="translate(50, 92)"
+                    strokeWidth="2"
+                    stroke=""
+                    fill="#1f2127"
+                    points="50,50 120.5,8 120.5,35 50,80"
+                  ></polygon>
+                </g>
+              </svg>
+            </div>
+            <div className="media-section">
+              <a href="https://www.linkedin.com/in/evan-verplaetse/" target="_blank" rel="noopener noreferrer">
+                <img src="https://img.icons8.com/color/48/000000/linkedin.png" alt="LinkedIn" />
+              </a>
+              <a href="https://www.instagram.com/evan_verplaetse/" target="_blank" rel="noopener noreferrer">
+                <img src="https://img.icons8.com/color/48/000000/instagram-new.png" alt="Instagram" />
+              </a>
+              <img src="https://img.icons8.com/color/48/000000/discord-logo.png" alt="Discord" />
+              <p>._._404_._.</p>
+            </div>
+            <div className="cv-section">
+            </div>
           </div>
+      <div className="container"></div>
         </div>
-      ) : (
-        <>
-          <header className="App-header">
-            <div className="terminal-title">Bienvenue sur mon Portfolio...</div>
-            <div className="honeycomb">
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-            </div>
-            <button className="burger-menu" onClick={toggleNav}>
-              ☰
-            </button>
-            <nav className={isNavVisible ? 'visible' : ''}>
-              <a href="#home">Accueil</a>
-              <a href="#about">À propos</a>
-              <a href="#projects">Projets</a>
-              <a href="#contact">Contact</a>
-            </nav>
-          </header>
-          <h1>Accueil</h1>
-          <main id="home" className="home-section">
-            <p className="hours">{currentTime.toLocaleTimeString('fr-FR', { hour12: false })}</p>
-            <div className="information-system">
-              <pre id="terminal-output">
-                <p>User: Evan_Verplaetse</p> <br />
-                <p>OS: Ubuntu 24.04 LTS</p> <br />
-                <p>CPU: Intel i7-10700K</p> <br />
-                <p>GPU: NVIDIA GeForce RTX 3060</p> <br />
-                <p>Version du projet: 1.0.0</p> <br />
-                <p>Dernière mise à jour: Jan 2025</p> <br />
-                <p>Disk Usage: 75% full</p> <br />
-                <p>Memory: 8GB RAM</p> <br />
-                <p>Connection: Online</p> <br />
-                <p className="cpu">CPU: {cpuUsage}%</p>
-                <p>Mémoire: {memoryUsage}%</p>
-              </pre>
-              <h2 className="info-system">Informations Système</h2>
-            </div>
-            <div className="system-info">
-              <div className="info-panel"></div>
-              <h2 className="file-manager-title">Gestionnaire de fichiers</h2>
-              <div className="file-manager">
-                <div className="file">amaze.wow</div>
-                <div className="file">archiv.txt</div>
-                <div className="file">src/</div>
-                <div className="file">easter.egg</div>
-                <div className="file">folio.jsx</div>
-                <div className="file">my_hunter.c</div>
-                <div className="file">readme.md</div>
-                <div className="file">.gitignore</div>
-                <div className="file">forkbomb.c</div>
-                <div className="file">file.json</div>
-                <div className="file">core.c</div>
-                <div className="file">neutrinos.py</div>
-              </div>
-            </div>
-            <div className="keyboard-container">
-              <div className="keyboard">
-                {keys.map((row, rowIndex) => (
-                  <div className="keyboard-row" key={rowIndex}>
-                    {row.map((key) => (
-                      <button
-                        key={key}
-                        className={`key ${pressedKey === key ? 'pressed' : ''}`}
-                        onClick={() => handleVirtualKeyPress(key)}
-                      >
-                        {key}
-                      </button>
-                    ))}
-                  </div>
-                ))}
-              </div>
-            </div>
-          </main>
-        </>
-      )}
-    </div>
   );
 }
 
